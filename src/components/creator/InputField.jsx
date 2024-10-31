7b742176762aa17b800c520ecdd1b49fccd6b3de
kittyismylife <chss0520@sookmyung.ac.kr>
2024-10-31 18:36:52 +0900

    ✨ Feat: span으로 부가 설명 추가 구현

--- src/components/creator/InputField.jsx
+++ src/components/creator/InputField.jsx
@@ -3,6 +3,8 @@ import styles from '../../styles/creator/CreatorRegistration.module.css';
 import { useState } from 'react';
 
 const InputField = ({
+  spanPosition,
+  span,
   label,
   value,
   setValue,
@@ -21,6 +23,7 @@ const InputField = ({
   return (
     <div className={styles.inputcontainer} style={{ maxWidth }}>
       <h3>{label}</h3>
+
       <input
         type="text"
         className={styles.input}
@@ -29,11 +32,16 @@ const InputField = ({
         value={value}
         onChange={checkContent}
       />
+      {spanPosition === 'back' && (
+        <span className={styles.spanBack}>{span}</span>
+      )}
     </div>
   );
 };
 
 InputField.propTypes = {
+  spanPosition: PropTypes.oneOf(['front', 'back']),
+  span: PropTypes.string,
   label: PropTypes.string.isRequired,
   value: PropTypes.string.isRequired,
   setValue: PropTypes.func.isRequired,
