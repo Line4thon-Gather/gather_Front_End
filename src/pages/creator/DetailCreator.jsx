082fcfa5fbab8d5417355ffaedeeb04a7ded5ce8
kittyismylife <chss0520@sookmyung.ac.kr>
2024-11-05 00:51:40 +0900

    ✨ Feat: 크리에이터 세부 내용 페이지 구현
    크리에이터 찾기 부분에서 카드 클릭 시 보일 세부 내용 페이지 구현 (완전 구현은 아니고, 일단 1차 구현 - 구현 중이었는데 로그인 먼저 쳐내는 것이 맞을 것 같아서 커밋 우선으로 해 둠)

---
+++ src/pages/creator/DetailCreator.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import DetailTop from '../../components/creator/DetailTop';
+import Nav from '../../components/common/Nav';
+import TabSliderMenu from '../../components/creator/TabSliderMenu';
+
+const DetailCreator = () => {
+  return (
+    <div>
+      <Nav />
+      <DetailTop />
+      <div style={{ backgroundColor: '#F6F8F9', marginTop: '40px' }}>
+        <TabSliderMenu />
+      </div>
+    </div>
+  );
+};
+
+export default DetailCreator;
