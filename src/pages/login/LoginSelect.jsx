6b79e1cd383da57c9d9219a206ee3c09f0ac616c
kittyismylife <chss0520@sookmyung.ac.kr>
2024-11-05 13:04:43 +0900

    ✨ Feat: 로그인 기본 페이지 구현
    대학생 / 창업 선택 및 버튼 하나 선택해야 선택 완료 버튼 작동

---
+++ src/pages/login/LoginSelect.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import Question from '../../components/login/Question.jsx';
+import OptionButton from '../../components/login/OptionButton.jsx';
+import NextButton from '../../components/login/NextButton.jsx';
+import styles from '../../styles/login/LoginSelect.module.css';
+
+const LoginSelect = () => {
+  const [selectedOption, setSelectedOption] = useState('');
+
+  const handleOptionSelect = (option) => {
+    if (selectedOption === option) {
+      setSelectedOption('');
+    } else {
+      setSelectedOption(option);
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <Question QuestionNum="Q.1" Question="어떤 상태이신가요?" />
+      <div className={styles.Btncontainer}>
+        <OptionButton
+          text="대학생이에요"
+          isSelected={selectedOption === '대학생이에요'}
+          onClick={() => handleOptionSelect('대학생이에요')}
+        />
+        <OptionButton
+          text="창업했어요"
+          isSelected={selectedOption === '창업했어요'}
+          onClick={() => handleOptionSelect('창업했어요')}
+        />
+      </div>
+      <NextButton
+        isEnabled={selectedOption !== ''}
+        onClick={() => alert(`Selected: ${selectedOption}`)}
+      />
+    </div>
+  );
+};
+
+export default LoginSelect;
