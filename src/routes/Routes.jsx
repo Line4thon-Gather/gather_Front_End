7853bf2d421afd0708b888a816b6220b37e2454b
YeChan Kim <az20058@naver.com>
2024-11-06 21:32:34 +0900

    ⚙ Settings : 라우팅 설정에 크리에이터 찾기 페이지 추가(/creator)

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { Routes as ReactRouters, Route, Outlet } from 'react-router-dom';
 import Home from '../pages/home/Home';
 import Strategy from '../pages/strategy/Strategy';
+import FindCreator from '../pages/creator/FindCreator';
 
 export default function Routes() {
   return (
@@ -8,6 +9,7 @@ export default function Routes() {
       <Route path="/" element={<Layout />}>
         <Route path="" element={<Home />} />
         <Route path="strategy" element={<Strategy />} />
+        <Route path="creator" element={<FindCreator />} />
       </Route>
     </ReactRouters>
   );
