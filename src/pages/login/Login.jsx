d1d64e0460ee9290c5603d7c8815207947417d5d
kittyismylife <chss0520@sookmyung.ac.kr>
2024-11-07 11:59:28 +0900

    ✨ Feat : 구글 소셜 로그인 버튼 추가

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,14 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from '../../styles/login/Login.module.css';
 import image from '../../assets/images/ModalImage.png';
 import google from '../../assets/images/google_button.png';
 
 const Login = () => {
-  const handleGoogleLogin = () => {
-    // 여기에 구글 로그인 함수 추가
-    console.log('Google 로그인 버튼 클릭됨');
-  };
-
   return (
     <div className={styles.container}>
       <div className={styles.loginBox}>
@@ -19,12 +14,10 @@ const Login = () => {
           한정된 자금 안에서 최대한의 부가가치를 창출할 수 있어요!
         </p>
 
-        <img
-          className={styles.googleButton}
-          src={google}
-          alt="Google Icon"
-          onClick={handleGoogleLogin}
-        />
+        {/* Google 소셜 로그인 링크 */}
+        <a href="https://backend.to-gather.info/oauth2/authorization/google">
+          <img className={styles.googleButton} src={google} alt="Google Icon" />
+        </a>
       </div>
     </div>
   );
