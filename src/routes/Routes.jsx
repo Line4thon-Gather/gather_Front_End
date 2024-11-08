9b1de82904c872338bb079303285e1c88ad641a1
kittyismylife <chss0520@sookmyung.ac.kr>
2024-11-08 20:18:20 +0900

    ✨ Feat : 라우트 추가

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,5 +1,10 @@
 import { Routes as ReactRouters, Route, Outlet } from 'react-router-dom';
 import Home from '../pages/home/Home';
+import Login from '../pages/login/Login';
+import LoginSelect from '../pages/login/LoginSelect';
+import Redirection from '../pages/login/Redirection';
+import StudentCeritifcation from '../pages/login/StudentCertification';
+import BusinessCertification from '../pages/login/BusinessCertification';
 import Strategy from '../pages/strategy/Strategy';
 import FindCreator from '../pages/creator/FindCreator';
 import ScrollToTop from '../hooks/ScrollUp';
@@ -11,6 +16,11 @@ export default function Routes() {
         <Route path="" element={<Home />} />
         <Route path="strategy" element={<Strategy />} />
         <Route path="creator" element={<FindCreator />} />
+        <Route path="login" element={<Login />} />
+        <Route path="login-select" element={<LoginSelect />} />
+        <Route path="redirect" element={<Redirection />} />
+        <Route path="student" element={<StudentCeritifcation />} />
+        <Route path="business" element={<BusinessCertification />} />
       </Route>
     </ReactRouters>
   );
