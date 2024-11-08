c670eae7f719949cf71f55b8a1d33a7b77116684
kittyismylife <chss0520@sookmyung.ac.kr>
2024-11-08 13:02:03 +0900

    ✨ Feat : 토큰 유지 추가 및 값 수정
    불필요한 주석 삭제
    ERD 참고해서 일부 변수명 수정

--- src/pages/login/LoginSelect.jsx
+++ src/pages/login/LoginSelect.jsx
@@ -1,27 +1,27 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // useNavigate 임포트
+import { useNavigate } from 'react-router-dom';
 import Question from '../../components/login/Question.jsx';
 import OptionButton from '../../components/login/OptionButton.jsx';
 import NextButton from '../../components/login/NextButton.jsx';
 import styles from '../../styles/login/LoginSelect.module.css';
 
 const LoginSelect = () => {
-  const [selectedOption, setSelectedOption] = useState('');
-  const navigate = useNavigate(); // useNavigate 훅 사용
+  const [role, setSelectedOption] = useState('');
+  const navigate = useNavigate();
 
   const handleOptionSelect = (option) => {
-    if (selectedOption === option) {
-      setSelectedOption('');
-    } else {
-      setSelectedOption(option);
-    }
+    setSelectedOption((prev) => (prev === option ? '' : option));
   };
 
   const handleNextClick = () => {
-    if (selectedOption === '대학생이에요') {
-      navigate('/student'); // 대학생 전용 페이지로 이동
-    } else if (selectedOption === '창업했어요') {
-      navigate('/business'); // 사업자 대상 페이지로 이동
+    if (role) {
+      localStorage.setItem('role', role);
+
+      if (role === '대학생') {
+        navigate('/student');
+      } else if (role === '창업') {
+        navigate('/business');
+      }
     }
   };
 
@@ -31,19 +31,19 @@ const LoginSelect = () => {
       <div className={styles.Btncontainer}>
         <OptionButton
           text="대학생이에요"
-          isSelected={selectedOption === '대학생이에요'}
-          onClick={() => handleOptionSelect('대학생이에요')}
+          isSelected={role === '대학생'}
+          onClick={() => handleOptionSelect('대학생')}
         />
         <OptionButton
           text="창업했어요"
-          isSelected={selectedOption === '창업했어요'}
-          onClick={() => handleOptionSelect('창업했어요')}
+          isSelected={role === '창업'}
+          onClick={() => handleOptionSelect('창업')}
         />
       </div>
       <NextButton
         text="선택 완료"
-        isEnabled={selectedOption !== ''}
-        onClick={handleNextClick} // 페이지 이동 함수 호출
+        isEnabled={role !== ''}
+        onClick={handleNextClick}
       />
     </div>
   );
