2567e7ae2106e4fdba68a1e4155c459a7b9bda55
kittyismylife <chss0520@sookmyung.ac.kr>
2024-11-09 11:27:47 +0900

    🎨 Design : Nav CSS 분리 및 수정

--- src/components/common/Nav.jsx
+++ src/components/common/Nav.jsx
@@ -1,76 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+import styles from '../../styles/common/Nav.module.css';
 import Image from '../../assets/images/NavImage.png';
 import basicImg from '../../assets/images/basicprofile.png';
-import { Link, NavLink } from 'react-router-dom';
-
-const NavBar = styled.nav`
-  display: flex;
-  width: 100%;
-  height: 60px;
-  align-items: center;
-  padding: 0 20px;
-  background-color: #27282d;
-`;
-
-const LogoLink = styled(NavLink)`
-  display: inline-block;
-  margin-right: 100px;
-`;
-
-const LogoImage = styled.img`
-  width: 147px;
-  height: 33.266px;
-`;
-
-const NavItems = styled.ul`
-  list-style: none;
-  display: flex;
-  gap: 60px;
-  padding: 0;
-  justify-content: center;
-  align-items: center;
-  margin-left: 400px;
-`;
-
-const NavItem = styled.li`
-  cursor: pointer;
-  color: #fff;
-  font-weight: bold;
-  padding: 0px 16px;
-`;
-
-const LoginBtn = styled.li`
-  cursor: pointer;
-  color: #fff;
-  font-weight: bold;
-  padding: 0px 16px;
-  margin-left: 130px;
-`;
-
-const UserInfo = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 15px;
-  margin-left: 130px;
-  color: #fff;
-`;
-
-const ProfileImage = styled.img`
-  width: 30px;
-  height: 30px;
-  border-radius: 50%;
-`;
-
-const activeStyle = {
-  color: '#4E96FF',
-  textDecoration: 'none',
-};
-
-const basicStyle = {
-  color: '#fff',
-  textDecoration: 'none',
-};
 
 const Nav = () => {
   const token = localStorage.getItem('token');
@@ -79,53 +11,65 @@ const Nav = () => {
   const userProfileImage = basicImg;
 
   return (
-    <NavBar>
-      <NavItems>
-        <LogoLink to="/">
-          <LogoImage src={Image} alt="로고" />
-        </LogoLink>
+    <nav className={styles.navBar}>
+      <ul className={styles.navItems}>
+        <NavLink to="/" className={styles.logoLink}>
+          <img src={Image} alt="로고" className={styles.logoImage} />
+        </NavLink>
 
-        <NavItem>
+        <li className={styles.navItem}>
           <NavLink
             to="/strategy"
-            style={({ isActive }) => (isActive ? activeStyle : basicStyle)}
+            className={({ isActive }) =>
+              isActive ? styles.activeStyle : styles.basicStyle
+            }
           >
             홍보 전략
           </NavLink>
-        </NavItem>
-        <NavItem>
+        </li>
+        <li className={styles.navItem}>
           <NavLink
             to="/creator"
-            style={({ isActive }) => (isActive ? activeStyle : basicStyle)}
+            className={({ isActive }) =>
+              isActive ? styles.activeStyle : styles.basicStyle
+            }
           >
             크리에이터 찾기
           </NavLink>
-        </NavItem>
-        <NavItem>
+        </li>
+        <li className={styles.navItem}>
           <NavLink
             to="/register"
-            style={({ isActive }) => (isActive ? activeStyle : basicStyle)}
+            className={({ isActive }) =>
+              isActive ? styles.activeStyle : styles.basicStyle
+            }
           >
             크리에이터 등록
           </NavLink>
-        </NavItem>
+        </li>
         {isLoggedIn ? (
-          <UserInfo>
-            <ProfileImage src={userProfileImage} alt="프로필 이미지" />
+          <div className={styles.userInfo}>
+            <img
+              src={userProfileImage}
+              alt="프로필 이미지"
+              className={styles.profileImage}
+            />
             <span>{userName}</span>
-          </UserInfo>
+          </div>
         ) : (
-          <LoginBtn>
+          <li className={styles.loginBtn}>
             <NavLink
               to="/login"
-              style={({ isActive }) => (isActive ? activeStyle : basicStyle)}
+              className={({ isActive }) =>
+                isActive ? styles.activeStyle : styles.basicStyle
+              }
             >
               로그인
             </NavLink>
-          </LoginBtn>
+          </li>
         )}
-      </NavItems>
-    </NavBar>
+      </ul>
+    </nav>
   );
 };
 
