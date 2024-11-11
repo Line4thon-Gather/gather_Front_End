c1b7af2a458a0cc1d61d117daf4b9296562f2b92
kittyismylife <chss0520@sookmyung.ac.kr>
2024-11-11 13:16:12 +0900

    ✨ Feat : 슬라이드에 해당하는 항목들 컴포넌트화

--- src/components/creator/TabSliderMenu.jsx
+++ src/components/creator/TabSliderMenu.jsx
@@ -1,30 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styles from '../../styles/creator/TabSliderMenu.module.css';
+import Introduction from './Introduction';
+// import Services from './Services';
+// import Contact from './Contact';
 
 const TabSliderMenu = () => {
   const [activeTab, setActiveTab] = useState(0);
-  const [userContent, setUserContent] = useState([]);
-
   const tabs = ['소개/포트폴리오', '작업 가능 항목/가격', '연락 문의'];
-
-  // 사용자 맞춤형 콘텐츠를 불러오는 함수 (예시로 하드코딩된 데이터 사용)
-  useEffect(() => {
-    // API 호출 또는 데이터베이스에서 사용자 콘텐츠를 가져오는 로직
-    const fetchUserContent = () => {
-      // 예시 데이터
-      const content = [
-        '사용자 소개 및 포트폴리오 내용',
-        '작업 가능 항목 및 가격 정보',
-        '연락 문의 방법에 대한 설명',
-      ];
-      setUserContent(content);
-    };
-
-    fetchUserContent();
-  }, []);
 
   const handleTabClick = (index) => {
     setActiveTab(index);
+  };
+
+  const renderContent = () => {
+    switch (activeTab) {
+      case 0:
+        return <Introduction />;
+      // case 1:
+      //   return <Services />;
+      // case 2:
+      //   return <Contact />;
+      default:
+        return null;
+    }
   };
 
   return (
@@ -43,9 +41,7 @@ const TabSliderMenu = () => {
         ))}
       </div>
       <div className={styles.slider}>
-        {userContent.length > 0 && (
-          <div className={styles.tabContent}>{userContent[activeTab]}</div>
-        )}
+        <div className={styles.tabContent}>{renderContent()}</div>
       </div>
     </div>
   );
