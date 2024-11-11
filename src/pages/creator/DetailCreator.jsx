e5ee6a63daa6498097d23350a934485b2444d5e2
kittyismylife <chss0520@sookmyung.ac.kr>
2024-11-11 13:16:48 +0900

    🎨 Design : 크리에이터 상세보기 디자인 UI 수정

--- src/pages/creator/DetailCreator.jsx
+++ src/pages/creator/DetailCreator.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import DetailTop from '../../components/creator/DetailTop';
-import Nav from '../../components/common/Nav';
 import TabSliderMenu from '../../components/creator/TabSliderMenu';
+import styles from '../../styles/creator/DetailCreator.module.css';
+import Footer from '../../components/home/Footer';
 
 const DetailCreator = () => {
   return (
     <div>
-      <DetailTop />
-      <div style={{ backgroundColor: '#F6F8F9', marginTop: '40px' }}>
-        <TabSliderMenu />
+      <div className={styles.container}>
+        <div className={styles.innercontainer}>
+          <div className={styles.white}>
+            <DetailTop />
+          </div>
+          <div className={styles.tabcontainer}>
+            <TabSliderMenu />
+          </div>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 };
