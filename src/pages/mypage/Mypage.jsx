bc95afa7a0d94fe91bcad7bbaeaf572287b459da
kittyismylife <chss0520@sookmyung.ac.kr>
2024-11-16 15:46:28 +0900

    ✨ Feat : 크리에이터명 받아오기

--- src/pages/mypage/Mypage.jsx
+++ src/pages/mypage/Mypage.jsx
@@ -9,10 +9,9 @@ import ThumbnailCard from '../../components/creator/ThumbnailCard';
 const Mypage = () => {
   const navigate = useNavigate();
   const [profileImage, setProfileImage] = useState(defaultProfile);
-  const [name, setName] = useState('');
+  const [nickname, setName] = useState('');
   const [email, setEmail] = useState('');
-  const [showModal, setShowModal] = useState(false);
-  const [modalMessage, setModalMessage] = useState('');
+  const [role, setRole] = useState('');
   const [recentCreators, setRecentCreators] = useState([]);
   const [loading, setLoading] = useState(true);
 
@@ -23,22 +22,10 @@ const Mypage = () => {
   };
 
   useEffect(() => {
-    const token = localStorage.getItem('token');
-    const userName = localStorage.getItem('userName') || '이름을 입력해주세요';
-    const userEmail =
-      localStorage.getItem('userEmail') || '이메일을 입력해주세요';
-    const userProfileImage =
-      localStorage.getItem('profileImgUrl') || defaultProfile;
-
-    if (token) {
-      setName(userName);
-      setEmail(userEmail);
-      setProfileImage(userProfileImage);
-    }
-    fetchRecentCreators();
+    fetchUserProfile();
   }, []);
 
-  const fetchRecentCreators = async () => {
+  const fetchUserProfile = async () => {
     try {
       const token = localStorage.getItem('token');
       if (!token) {
@@ -57,99 +44,32 @@ const Mypage = () => {
 
       if (response.data.isSuccess && response.data.data) {
         const { profileInfo, creatorInfo } = response.data.data;
-        setRecentCreators(creatorInfo || []);
+
         setProfileImage(profileInfo.profileImgUrl || defaultProfile);
-        setEmail(profileInfo.email || '이메일을 입력해주세요');
+        setName(profileInfo.nickname || '이름 없음');
+        setEmail(profileInfo.email || '이메일 없음');
+        setRole(profileInfo.role || '');
+        setRecentCreators(creatorInfo || []);
       } else {
         throw new Error(
           response.data.message || 'Unexpected response structure'
         );
       }
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error('Error fetching user profile:', error);
       setRecentCreators([]);
     } finally {
       setLoading(false);
     }
   };
 
+  const handleLogout = () => {
+    localStorage.removeItem('token'); // 토큰 삭제
+    navigate('/login'); // 로그인 페이지로 리다이렉트
+  };
+
   const handleThumbnailClick = (nickname) => {
     navigate(`/creator/${nickname}`);
-  };
-
-  const handleImageChange = (e) => {
-    if (e.target.files && e.target.files[0]) {
-      const newImageURL = URL.createObjectURL(e.target.files[0]);
-      setProfileImage(newImageURL);
-      localStorage.setItem('profileImgUrl', newImageURL);
-    }
-  };
-
-  const handleNameChange = (e) => {
-    setName(e.target.value);
-  };
-
-  const handleNameBlur = () => {
-    if (name.trim() === '') {
-      setModalMessage('이름을 빈 문자열로 설정할 수 없습니다.');
-      setShowModal(true);
-      const storedUserName =
-        localStorage.getItem('userName') || '이름을 입력해주세요';
-      setName(storedUserName);
-    } else {
-      localStorage.setItem('userName', name);
-    }
-  };
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handleEmailBlur = () => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const koreanRegex = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
-    const storedUserEmail =
-      localStorage.getItem('userEmail') || '이메일을 설정해주세요.';
-
-    if (email.trim() === '') {
-      setModalMessage('이메일을 빈 문자열로 설정할 수 없습니다.');
-      setShowModal(true);
-      setEmail(storedUserEmail);
-    } else if (!emailRegex.test(email) || koreanRegex.test(email)) {
-      setModalMessage(
-        koreanRegex.test(email)
-          ? '이메일에 한글을 포함할 수 없습니다.'
-          : '유효하지 않은 이메일 형식입니다.'
-      );
-      setShowModal(true);
-      setEmail(storedUserEmail);
-    } else {
-      localStorage.setItem('userEmail', email);
-    }
-  };
-
-  const closeModal = () => {
-    setShowModal(false);
-  };
-
-  const handleLogout = () => {
-    localStorage.removeItem('token');
-    localStorage.removeItem('userName');
-    localStorage.removeItem('userEmail');
-    localStorage.removeItem('profileImgUrl');
-    localStorage.removeItem('isRegistered');
-    localStorage.removeItem('role');
-    navigate('/login');
-  };
-
-  const handleAccountDelete = () => {
-    const confirmDelete = window.confirm(
-      '정말로 회원탈퇴를 진행하시겠습니까? 이 작업은 되돌릴 수 없습니다.'
-    );
-    if (confirmDelete) {
-      localStorage.clear();
-      navigate('/');
-    }
   };
 
   return (
@@ -161,47 +81,27 @@ const Mypage = () => {
             <div className={styles.basicProfile}>
               <div className={styles.profileImageContainer}>
                 <p className={styles.profileImageLabel}>프로필 이미지</p>
-                <label htmlFor="profileImageInput">
-                  <img
-                    src={profileImage}
-                    alt="Profile"
-                    className={styles.profileImage}
-                  />
-                </label>
-                <input
-                  id="profileImageInput"
-                  type="file"
-                  accept="image/*"
-                  onChange={handleImageChange}
-                  className={styles.hiddenInput}
+                <img
+                  src={profileImage}
+                  alt="Profile"
+                  className={styles.profileImage}
                 />
               </div>
               <div className={styles.profileContainer}>
                 <div className={styles.profileInfo}>
-                  <label htmlFor="nameInput" className={styles.inputLabel}>
+                  <label className={styles.inputLabel2}>
                     이름
+                    {role === '크리에이터' && (
+                      <div className={styles.creator}>
+                        <p>크리에이터</p>
+                      </div>
+                    )}
                   </label>
-                  <input
-                    id="nameInput"
-                    type="text"
-                    value={name}
-                    onChange={handleNameChange}
-                    onBlur={handleNameBlur}
-                    className={styles.ChangeInput}
-                  />
+                  <label className={styles.ChangeInput}>{nickname}</label>
                 </div>
                 <div className={styles.profileInfo}>
-                  <label htmlFor="emailInput" className={styles.inputLabel}>
-                    이메일
-                  </label>
-                  <input
-                    id="emailInput"
-                    type="email"
-                    value={email}
-                    onChange={handleEmailChange}
-                    onBlur={handleEmailBlur}
-                    className={styles.ChangeInput}
-                  />
+                  <label className={styles.inputLabel}>이메일</label>
+                  <label className={styles.ChangeInput}>{email}</label>
                 </div>
               </div>
             </div>
@@ -239,27 +139,15 @@ const Mypage = () => {
               )}
             </div>
           </div>
-
-          {showModal && (
-            <div className={styles.modal}>
-              <div className={styles.modalContent}>
-                <h2>{modalMessage}</h2>
-                <button onClick={closeModal} className={styles.closeButton}>
-                  닫기
-                </button>
-              </div>
-            </div>
-          )}
         </div>
       </div>
+
       <div className={styles.buttonContainer}>
-        <button onClick={handleLogout} className={styles.clickButton}>
+        <button className={styles.clickButton} onClick={handleLogout}>
           로그아웃
         </button>
-        <button onClick={handleAccountDelete} className={styles.clickButton}>
-          회원탈퇴
-        </button>
       </div>
+
       <Footer />
     </>
   );
